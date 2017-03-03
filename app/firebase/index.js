723892971039b467689f6312f2139d98639fc541
Phillip Eade <Phillip Eade>
2017-03-03 19:03:30 +1100

    new firebase database

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,16 +2,17 @@ import firebase from 'firebase';
 
 try{
    var config = {
-      apiKey: "AIzaSyAr-3iMexzPGko6M3B5sMownl1IjnAC-TA",
-      authDomain: "phillips-todo-app.firebaseapp.com",
-      databaseURL: "https://phillips-todo-app.firebaseio.com",
-      storageBucket: "phillips-todo-app.appspot.com",
-      messagingSenderId: "420017912822"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
+    console.log(config);
     firebase.initializeApp(config);
 } catch(e) {
 
-};
- 
+}
+
 export var firebaseRef=firebase.database().ref();
 export default firebase;
