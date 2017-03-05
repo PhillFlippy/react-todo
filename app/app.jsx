dbf6b98dd06a845349df387d4c0006ee9285e82a
Phillip Eade <Phillip Eade>
2017-03-05 14:23:44 +1100

    Add redirects and private pages

--- app/app.jsx
+++ app/app.jsx
@@ -1,13 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {hashHistory} = require('react-router');
 
-import TodoApp from 'ToDoApp';
 var {Provider} = require('react-redux');
 var actions = require('actions');
 var store = require('configureStore').configure();
-var TodoAPI = require('TodoAPI');
-import Login from 'Login';
+import firebase from 'app/firebase/';
+import router from 'app/router/';
+
+firebase.auth().onAuthStateChanged((user) => {
+  if(user){
+    hashHistory.push('/todos');
+  } else {
+hashHistory.push('/');
+  }
+});
+
 // store.subscribe(() =>{
 //   var state = store.getState()
 //   console.log('New state', store.getState());
@@ -16,20 +24,19 @@ import Login from 'Login';
 // var initialTodos = TodoAPI.getTodos();
 // store.dispatch(actions.addTodos(initialTodos));
 
+
+
 store.dispatch(actions.startAddTodos());
 
 //load foundation
 $(document).foundation();
 //App css
 require('style!css!sass!applicationStyles')
+
+
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={hashHistory}>
-    <Route path="/">
-    <Route path='todos' component={TodoApp} />
-    <IndexRoute component={Login} />
-  </Route>
-  </Router>
+      {router}
   </Provider>,
   document.getElementById('app')
 );
